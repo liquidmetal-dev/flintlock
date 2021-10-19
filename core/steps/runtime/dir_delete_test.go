d3d0489be38a59e8c937871c9c050d4d67c44599
Balazs Nadasdi <balazs@weave.works>
2021-10-19 16:58:41 +0200

    Testing
    * Create plan test
    * Delete plan test
    
    * core/steps/runtime test

---
+++ core/steps/runtime/dir_delete_test.go
@@ -0,0 +1,50 @@
+package runtime_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/onsi/gomega"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"github.com/weaveworks/reignite/core/steps/runtime"
+)
+
+func TestDeleteDirectory_NotExists(t *testing.T) {
+	gomega.RegisterTestingT(t)
+
+	testDir := "/test/or/not-to-test"
+
+	fs := afero.NewMemMapFs()
+	ctx := context.Background()
+
+	step := runtime.NewDeleteDirectory(testDir, fs)
+	should, shouldErr := step.ShouldDo(ctx)
+	extraSteps, doErr := step.Do(ctx)
+
+	assert.NoError(t, shouldErr)
+	assert.False(t, should)
+	assert.NoError(t, doErr)
+	assert.Empty(t, extraSteps)
+}
+
+func TestDeleteDirectory_Exists(t *testing.T) {
+	gomega.RegisterTestingT(t)
+
+	testDir := "/test/or/not-to-test"
+
+	fs := afero.NewMemMapFs()
+	ctx := context.Background()
+
+	fs.MkdirAll(testDir, os.ModeDir)
+
+	step := runtime.NewDeleteDirectory(testDir, fs)
+	should, shouldErr := step.ShouldDo(ctx)
+	extraSteps, doErr := step.Do(ctx)
+
+	assert.NoError(t, shouldErr)
+	assert.True(t, should)
+	assert.NoError(t, doErr)
+	assert.Empty(t, extraSteps)
+}
