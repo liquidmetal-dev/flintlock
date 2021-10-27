75dba9dbd45f740da1a541ef981168fa33d3f21b
Josh <github@mickey.dev>
2021-10-27 11:25:08 +0000

    feat: add list microvm api implementation

--- core/application/query.go
+++ core/application/query.go
@@ -36,5 +36,23 @@ func (a *app) GetMicroVM(ctx context.Context, id, namespace string) (*models.Mic
 }
 
 func (a *app) GetAllMicroVM(ctx context.Context, namespace string) ([]*models.MicroVM, error) {
-	return nil, errNotImplemeted
+	logger := log.GetLogger(ctx).WithField("component", "app")
+	logger.Trace("querying all microvms in namespace: ", namespace)
+
+	if namespace == "" {
+		return nil, errNamespaceRequired
+	}
+
+	foundMvms, err := a.ports.Repo.GetAll(ctx, namespace)
+	if err != nil {
+		return nil, fmt.Errorf("error attempting to list microvms in namespace: %s: %w", namespace, err)
+	}
+
+	if foundMvms == nil {
+		logger.Trace("no microvms were found in namespace: ", namespace)
+
+		return []*models.MicroVM{}, nil
+	}
+
+	return foundMvms, nil
 }
