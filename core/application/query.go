a41b21a9ff5a3a2101edd803af74e685f9d25ed3
Josh <github@mickey.dev>
2021-10-27 11:02:10 +0000

    feat: implement single microvm query API

--- core/application/query.go
+++ core/application/query.go
@@ -2,12 +2,37 @@ package application
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/weaveworks/flintlock/core/models"
+	"github.com/weaveworks/flintlock/pkg/log"
 )
 
 func (a *app) GetMicroVM(ctx context.Context, id, namespace string) (*models.MicroVM, error) {
-	return nil, errNotImplemeted
+	logger := log.GetLogger(ctx).WithField("component", "app")
+	logger.Tracef("querying microvm: %s, with namespace: %s", id, namespace)
+
+	if id == "" {
+		return nil, errIDRequired
+	}
+
+	if namespace == "" {
+		return nil, errNamespaceRequired
+	}
+
+	foundMvm, err := a.ports.Repo.Get(ctx, id, namespace)
+	if err != nil {
+		return nil, fmt.Errorf("error attempting to locate microvm with id: %s, in namespace: %s: %w", id, namespace, err)
+	}
+
+	if foundMvm == nil {
+		return nil, specNotFoundError{
+			name:      id,
+			namespace: namespace,
+		}
+	}
+
+	return foundMvm, nil
 }
 
 func (a *app) GetAllMicroVM(ctx context.Context, namespace string) ([]*models.MicroVM, error) {
