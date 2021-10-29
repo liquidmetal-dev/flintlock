af7fa95c53a6506f45bf566abab963e852f0bbee
Claudia Beresford <claudiaberesford@gmail.com>
2021-10-29 15:28:22 +0100

    Initial e2e CRUD tests

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -4,106 +4,165 @@
 package e2e_test
 
 import (
+	"context"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"os"
-	"path/filepath"
+	"strconv"
+	"syscall"
 	"testing"
-	"time"
 
 	. "github.com/onsi/gomega"
-
-	"github.com/gruntwork-io/terratest/modules/environment"
-	"github.com/gruntwork-io/terratest/modules/retry"
-	"github.com/gruntwork-io/terratest/modules/ssh"
-	"github.com/gruntwork-io/terratest/modules/terraform"
-	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/weaveworks/flintlock/api/services/microvm/v1alpha1"
+	"github.com/weaveworks/flintlock/api/types"
+	"github.com/weaveworks/flintlock/core/models"
+	ctr "github.com/weaveworks/flintlock/infrastructure/containerd"
+	"github.com/weaveworks/flintlock/test/e2e/utils"
 )
 
-func TestBasicScenario(t *testing.T) {
+func TestE2E(t *testing.T) {
 	RegisterTestingT(t)
 
-	terraDir := "./infra"
+	var (
+		mvmID  = "mvm0"
+		mvmNS  = "ns0"
+		fcPath = "/var/lib/flintlock/vm/%s/%s"
+	)
 
-	defer test_structure.RunTestStage(t, "teardown", func() {
-		terraformOptions := test_structure.LoadTerraformOptions(t, terraDir)
-		terraform.Destroy(t, terraformOptions)
-		test_structure.CleanupTestDataFolder(t, terraDir)
-	})
+	// TODO rename
+	r := utils.Runner{}
+	defer func() {
+		log.Println("TEST STEP: cleaning up running processes")
+		r.Teardown()
+	}()
+	log.Println("TEST STEP: performing setup, starting flintlockd server")
+	flintlockClient := r.Setup()
 
-	test_structure.RunTestStage(t, "setup", func() {
-		terraformOptions := configureTerraformOptionsAndSave(t, terraDir)
-
-		// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-		terraform.InitAndApply(t, terraformOptions)
-	})
-
-	test_structure.RunTestStage(t, "validate", func() {
-		terraformOptions := test_structure.LoadTerraformOptions(t, terraDir)
-		keyPair := test_structure.LoadSshKeyPair(t, terraDir)
-
-		testSSHToHost(t, terraformOptions, keyPair)
-	})
-}
-
-func testSSHToHost(t *testing.T, terraformOptions *terraform.Options, keyPair *ssh.KeyPair) {
-	publicIPAddress := terraform.Output(t, terraformOptions, "public_ip")
-	Expect(publicIPAddress).NotTo(BeEmpty())
-
-	publicHost := ssh.Host{
-		Hostname:    publicIPAddress,
-		SshKeyPair:  keyPair,
-		SshUserName: "root",
+	log.Println("TEST STEP: creating MicroVM")
+	createReq := v1alpha1.CreateMicroVMRequest{
+		Microvm: defaultTestMicroVM(mvmID, mvmNS),
 	}
+	created, err := flintlockClient.CreateMicroVM(context.Background(), &createReq)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(created.Microvm.Id).To(Equal(mvmID))
 
-	maxRetries := 30
-	timeBetweenRetries := 5 * time.Second
-	description := fmt.Sprintf("SSH to host %s", publicIPAddress)
-	command := "cat /tmp/metadata"
-
-	retry.DoWithRetry(t, description, maxRetries, timeBetweenRetries, func() (string, error) {
-		actualText, err := ssh.CheckSshCommandE(t, publicHost, command)
-		if err != nil {
-			return "", err
-		}
-
-		Expect(actualText).ToNot(BeEmpty())
-		return "", nil
+	// So all of this is a placeholder, just to verify that we are creating _something_.
+	// Once Get has been implemented, all/most of this can be replaced with 'Eventually' calls
+	// to check that the state of the mVM is running (which presumably will be somewhere
+	// in our recorded instance state returned by that call).
+	log.Println("TEST STEP: getting (and verifying) existing MicroVM")
+	repo, err := ctr.NewMicroVMRepo(&ctr.Config{
+		SocketPath: utils.ContainerdSocket,
+		Namespace:  "flintlock",
 	})
+	Expect(err).NotTo(HaveOccurred())
+
+	var fcPid int
+	Eventually(func(g Gomega) error {
+		// verify that the socket exists
+		g.Expect(fmt.Sprintf(fcPath, mvmNS, mvmID) + "/firecracker.sock").To(BeAnExistingFile())
+
+		// verify that firecracker has started and that a pid has been saved
+		contents, err := os.ReadFile(fmt.Sprintf(fcPath, mvmNS, mvmID) + "/firecracker.pid")
+		g.Expect(err).NotTo(HaveOccurred())
+		str := string(contents)
+		g.Expect(str).ToNot(BeEmpty())
+
+		// verify that there is actually a running process
+		// the main check here is actually in delete to ensure we have killed the process
+		fcPid, err = strconv.Atoi(str)
+		g.Expect(err).NotTo(HaveOccurred())
+		p, err := os.FindProcess(fcPid)
+		g.Expect(err).NotTo(HaveOccurred())
+		Expect(p.Signal(syscall.SIGCONT)).To(Succeed())
+
+		// check state according to containerd
+		// I would have liked to call the socket to verify the state of the instance from itself, but
+		// unfortunately when I try to do that here it casues both things writing to that socket
+		// to fail with "broken pipe".
+		// I want to ensure the mVM is _actually_ running before we call delete because
+		// otherwise if the delete call runs too early (before Save) then it is lost
+		// as we don't have requeueing done yet.
+		// So it was either this or a Sleep :D
+		model, err := repo.Get(context.Background(), mvmID, mvmNS)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(string(model.Status.State)).To(Equal(models.CreatedState))
+		return nil
+	}, "120s").Should(Succeed())
+
+	log.Println("TEST STEP: deleting existing MicroVM")
+	deleteReq := v1alpha1.DeleteMicroVMRequest{
+		Id:        mvmID,
+		Namespace: mvmNS,
+	}
+	_, err = flintlockClient.DeleteMicroVM(context.Background(), &deleteReq)
+	Expect(err).NotTo(HaveOccurred())
+
+	Eventually(func(g Gomega) error {
+		// verify that the vm state dir has been removed
+		g.Expect(fmt.Sprintf(fcPath, mvmNS, mvmID)).ToNot(BeAnExistingFile())
+
+		// verify that the firecracker process is no longer running
+		p, err := os.FindProcess(fcPid)
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(p.Signal(syscall.SIGCONT)).ToNot(Succeed())
+
+		// verify that the lease has been removed from containerd content store
+		_, err = repo.Get(context.Background(), mvmID, mvmNS)
+		g.Expect(err).To(MatchError(fmt.Sprintf("microvm spec %s/%s not found", mvmNS, mvmID)))
+		return nil
+	}, "120s").Should(Succeed())
 }
 
-func configureTerraformOptionsAndSave(t *testing.T, dir string) *terraform.Options {
-	// NOTE: ioutil has been used in places to write the file so that the contents aren't output to the logs
-	// Generate and save the options the kaypair
-	keyPair := ssh.GenerateRSAKeyPair(t, 4096)
-	test_structure.SaveSshKeyPair(t, dir, keyPair)
-	publicKeyPath := test_structure.FormatTestDataPath(dir, "id_rsa.pub")
-	ioutil.WriteFile(publicKeyPath, []byte(keyPair.PublicKey), os.ModePerm)
-	privateKeyPath := test_structure.FormatTestDataPath(dir, "id_rsa")
-	ioutil.WriteFile(privateKeyPath, []byte(keyPair.PrivateKey), os.ModePerm)
+func defaultTestMicroVM(name, namespace string) *types.MicroVMSpec {
+	var (
+		kernelImage = "docker.io/richardcase/ubuntu-bionic-kernel:0.0.11"
+		cloudImage  = "docker.io/richardcase/ubuntu-bionic-test:cloudimage_v0.0.1"
+	)
 
-	// Save the API key
-	apiToken := environment.GetFirstNonEmptyEnvVarOrFatal(t, []string{"METAL_AUTH_TOKEN"})
-	apiTokenPath := test_structure.FormatTestDataPath(dir, "api_token")
-	ioutil.WriteFile(apiTokenPath, []byte(apiToken), os.ModePerm)
-
-	// Get absolute paths
-	apiTokenPathAbs, _ := filepath.Abs(apiTokenPath)
-	privateKeyPathAbs, _ := filepath.Abs(privateKeyPath)
-	publicKeyPathAbs, _ := filepath.Abs(publicKeyPath)
-
-	// Create and save the options
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: dir,
-		Vars: map[string]interface{}{
-			"ssh_user":         "root",
-			"auth_token_path":  apiTokenPathAbs,
-			"public_key_path":  publicKeyPathAbs,
-			"private_key_path": privateKeyPathAbs,
+	return &types.MicroVMSpec{
+		Id:         name,
+		Namespace:  namespace,
+		Vcpu:       2,
+		MemoryInMb: 2048,
+		Kernel: &types.Kernel{
+			Image:            kernelImage,
+			Cmdline:          "console=ttyS0 reboot=k panic=1 pci=off i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd ds=nocloud-net;s=http://169.254.169.254/latest/",
+			Filename:         pointyString("vmlinux"),
+			AddNetworkConfig: true,
 		},
-		NoColor: true,
-	})
-	test_structure.SaveTerraformOptions(t, dir, terraformOptions)
+		Initrd: &types.Initrd{
+			Image:    kernelImage,
+			Filename: pointyString("initrd-generic"),
+		},
+		Volumes: []*types.Volume{{
+			Id:         "root",
+			IsRoot:     false,
+			IsReadOnly: true,
+			MountPoint: "/",
+			Source: &types.VolumeSource{
+				ContainerSource: pointyString(cloudImage),
+			}},
+		},
+		Interfaces: []*types.NetworkInterface{{
+			GuestDeviceName:  "eth0",
+			Type:             1,
+			AllowMetadataReq: true,
+			GuestMac:         pointyString("AA:FF:00:00:00:01"),
+			Address:          pointyString("169.254.0.1/16"),
+		},
+			{
+				GuestDeviceName:  "eth1",
+				Type:             0,
+				AllowMetadataReq: false,
+			}},
+		Metadata: map[string]string{
+			"meta-data": "aW5zdGFuY2VfaWQ6IG5zMS9tdm0wCmxvY2FsX2hvc3RuYW1lOiBtdm0wCnBsYXRmb3JtOiBsaXF1aWRfbWV0YWwK",
+			"user-data": "I2Nsb3VkLWNvbmZpZwpob3N0bmFtZTogbXZtMApmcWRuOiBtdm0wLmZydWl0Y2FzZQp1c2VyczoKICAgIC0gbmFtZTogcm9vdAogICAgICBzc2hfYXV0aG9yaXplZF9rZXlzOgogICAgICAgIC0gfAogICAgICAgICAgc3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUdzbStWSSsyVk5WWFBDRmVmbFhrQTVKY21zMzByajFGUFFjcFNTdDFrdVYgcmljaGFyZEB3ZWF2ZS53b3JrcwpkaXNhYmxlX3Jvb3Q6IGZhbHNlCnBhY2thZ2VfdXBkYXRlOiBmYWxzZQpmaW5hbF9tZXNzYWdlOiBUaGUgcmVpZ25pdGVkIGJvb3RlZCBzeXN0ZW0gaXMgZ29vZCB0byBnbyBhZnRlciAkVVBUSU1FIHNlY29uZHMKcnVuY21kOgogICAgLSBkaGNsaWVudCAtcgogICAgLSBkaGNsaWVudAo=",
+		},
+	}
+}
 
-	return terraformOptions
+func pointyString(v string) *string {
+	return &v
 }
---
+++ test/e2e/utils/runner.go
@@ -0,0 +1,126 @@
+//go:build e2e
+// +build e2e
+
+package utils
+
+import (
+	"bufio"
+	"errors"
+	"os/exec"
+	"strings"
+
+	gk "github.com/onsi/ginkgo"
+	gm "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+	"github.com/weaveworks/flintlock/api/services/microvm/v1alpha1"
+	"google.golang.org/grpc"
+)
+
+const (
+	containerdBin    = "containerd"
+	flintlockCmdDir  = "github.com/weaveworks/flintlock/cmd/flintlockd"
+	ContainerdSocket = "/run/containerd-dev/containerd.sock"
+	containerdCfg    = "/etc/containerd/config-dev.toml"
+	grpcDialTarget   = "127.0.0.1:9090"
+)
+
+// Runner is a very poorly named thing and honestly idk what to call it.
+// What it does is compile flintlockd and start containerd and flintlockd.
+// So 'TestSetterUpper' did not sound as slick, but that is what it is.
+// I am happy for literally any suggestions.
+type Runner struct {
+	flintlockdBin     string
+	containerdSession *gexec.Session
+	flintlockdSession *gexec.Session
+	flintlockdConn    *grpc.ClientConn
+}
+
+// Setup is a helper for the e2e tests which:
+// - compiles flintlockd
+// - starts containerd
+// - starts flintlockd
+// - opens a connection to the grpc server
+// - returns a new MicroVMClient which can then be used in testing
+// All opened connections and started processes are saved for later shutdown.
+// Teardown should be called before Setup in a defer.
+func (r *Runner) Setup() v1alpha1.MicroVMClient {
+	r.buildFLBinary()
+	r.startContainerd()
+	r.startFlintlockd()
+	r.dialGRPCServer()
+
+	return v1alpha1.NewMicroVMClient(r.flintlockdConn)
+}
+
+// Teardown will gracefully close and kill all connections and processes which were
+// opened as part of Setup.
+// It should be called before Setup as part of a defer:
+//
+// r := utils.Runner{}
+// defer r.Teardown()
+// r.Setup()
+// .
+func (r *Runner) Teardown() {
+	if r.flintlockdConn != nil {
+		r.flintlockdConn.Close()
+	}
+
+	if r.flintlockdSession != nil {
+		r.flintlockdSession.Terminate().Wait()
+	}
+
+	if r.containerdSession != nil {
+		r.containerdSession.Terminate().Wait()
+	}
+
+	gexec.CleanupBuildArtifacts()
+}
+
+func (r *Runner) buildFLBinary() {
+	flBin, err := gexec.Build(flintlockCmdDir)
+	gm.Expect(err).NotTo(gm.HaveOccurred())
+	r.flintlockdBin = flBin
+}
+
+func (r *Runner) startContainerd() {
+	ctrdCmd := exec.Command(containerdBin, "--config", containerdCfg)
+	ctrdSess, err := gexec.Start(ctrdCmd, gk.GinkgoWriter, gk.GinkgoWriter)
+	gm.Expect(err).NotTo(gm.HaveOccurred())
+	r.containerdSession = ctrdSess
+}
+
+func (r *Runner) startFlintlockd() {
+	parentIface, err := getParentInterface()
+	gm.Expect(err).NotTo(gm.HaveOccurred())
+	flCmd := exec.Command(r.flintlockdBin, "run", "--containerd-socket", ContainerdSocket, "--parent-iface", parentIface) //nolint:gosec
+	flSess, err := gexec.Start(flCmd, gk.GinkgoWriter, gk.GinkgoWriter)
+	gm.Expect(err).NotTo(gm.HaveOccurred())
+	r.flintlockdSession = flSess
+}
+
+func (r *Runner) dialGRPCServer() {
+	conn, err := grpc.Dial(grpcDialTarget, grpc.WithInsecure(), grpc.WithBlock())
+	gm.Expect(err).NotTo(gm.HaveOccurred())
+	r.flintlockdConn = conn
+}
+
+func getParentInterface() (string, error) {
+	// If there is a go package which lets me do this without shelling out lmk.
+	// I could not find one after a quick search.
+	cmd := exec.Command("ip", "route", "show")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	scanner := bufio.NewScanner(strings.NewReader(string(output)))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "default") {
+			parts := strings.Split(line, " ")
+
+			return parts[4], nil
+		}
+	}
+
+	return "", errors.New("parent interface not found") //nolint:goerr113
+}
