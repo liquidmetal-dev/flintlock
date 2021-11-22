dd1c7898a7ba2c887127b8a454831c32343ca15f
Balazs Nadasdi <balazs@weave.works>
2021-11-22 13:34:36 +0100

    Add a nice and friendly comment on ImageService integration test

--- infrastructure/containerd/image_service_integration_test.go
+++ infrastructure/containerd/image_service_integration_test.go
@@ -26,12 +26,63 @@ const (
 	testContainerdNS   = "flintlock_test_ctr"
 )
 
+// What this test does?
+//
+// == Preface
+//
+// Prepare ImageService and a Namespace context. All images and snapshots will
+// live under the same namespace.
+//
+// We do some prudent cleanup steps in case the test was aborted. Because it's
+// an integration test with an external service, if we don't clean up after
+// ourselves, the next test run will most likely fail. In theory, releasing the
+// leases should be enough, but that's heavily depend on how containerd tells
+// us how it works, so it's easier to delete everything we intended to create.
+//
+// == Chapter I: Pull and mount
+//
+// Using ImageService, we tries to Pull an image.
+//
+// We don't have separate Mount function (yet), we try to PullAndMount, with
+// this, we can be sure ImageService can pull and image from a repository and
+// can mount it.
+//
+// To avoid false positive result, we try to PullAndMount an image, we know
+// it's definitely not there, and we expect it to be failed.  If it does not
+// fail, something fishy is swimming under the hood.
+//
+// After PullAndMount, the real image should be mounted, but the fake one
+// shouldn't be. At the end we should have only one image available locally.
+//
+// == Chapter II: Snapshots
+//
+// As we already pull and mounted an image, we expect one snapshot to be there
+// with the name constructed from the VM namespace, the VM name, and the volume
+// usage type.
+// At the same time, we expect one lease to be there with similar naming
+// structure except the volume usage type we have one lease for all resources
+// in containerd.
+//
+// == Chapter III: Kernel
+//
+// We do the same checks for kernel. The kernel and volume works the same way,
+// and the real reason why we test it here because the usage type is different,
+// and we should be able to pull and mount them.
+//
+// == Chapter IV: Delete
+//
+// We should be able to delete both the volume and the kernel image. After we
+// delete them, we should see zero images in containerd.
 func TestImageService_Integration(t *testing.T) {
 	if !runContainerDTests() {
 		t.Skip("skipping containerd image service integration test")
 	}
 
 	RegisterTestingT(t)
+
+	//
+	// Preface
+	//
 
 	client, ctx := testCreateClient(t)
 	namespaceCtx := namespaces.WithNamespace(ctx, testContainerdNS)
@@ -71,6 +122,10 @@ func TestImageService_Integration(t *testing.T) {
 		}
 	}()
 
+	//
+	// Chapter I: Pull and mount.
+	//
+
 	err := imageSvc.Pull(ctx, inputGet)
 	Expect(err).NotTo(HaveOccurred())
 
@@ -108,6 +163,10 @@ func TestImageService_Integration(t *testing.T) {
 	Expect(len(img)).To(Equal(1))
 	Expect(img[0].Name).To(Equal(getTestVolumeImage()))
 
+	//
+	// Chapter II: Snapshots
+	//
+
 	snapshotExists := false
 	err = client.SnapshotService(testSnapshotter).Walk(namespaceCtx, func(walkCtx context.Context, info snapshots.Info) error {
 		if info.Name == expectedSnapshotName {
@@ -122,6 +181,10 @@ func TestImageService_Integration(t *testing.T) {
 	leases, err := client.LeasesService().List(namespaceCtx)
 	Expect(len(leases)).To(Equal(1))
 	Expect(leases[0].ID).To(Equal(expectedLeaseName), "expect lease with name %s to exists", expectedLeaseName)
+
+	//
+	// Chapter III: Kernel
+	//
 
 	inputGet.ImageName = getTestKernelImage()
 
@@ -145,6 +208,10 @@ func TestImageService_Integration(t *testing.T) {
 	Expect(mounts).NotTo(BeNil())
 	Expect(len(mounts)).To(Equal(1))
 
+	//
+	// Chapter IV: Delete
+	//
+
 	err = client.ImageService().Delete(namespaceCtx, getTestKernelImage())
 	Expect(err).NotTo(HaveOccurred())
 
@@ -157,6 +224,10 @@ func TestImageService_Integration(t *testing.T) {
 	})
 	Expect(err).ToNot(HaveOccurred())
 	Expect(exists).To(BeFalse())
+
+	images, err := client.ImageService().List(namespaceCtx)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(len(images)).To(BeZero())
 }
 
 func testCreateClient(t *testing.T) (*ctr.Client, context.Context) {
@@ -178,6 +249,8 @@ func runContainerDTests() bool {
 	return testCtr != ""
 }
 
+// getTestVolumeImage returns with the default test volume image name, if
+// CTR_TEST_VOL_IMG environment variable is not defined.
 func getTestVolumeImage() string {
 	envImage := os.Getenv("CTR_TEST_VOL_IMG")
 	if envImage != "" {
@@ -187,6 +260,8 @@ func getTestVolumeImage() string {
 	return testImageVolume
 }
 
+// getTestKernelImage returns with the default test kernel image name, if
+// CTR_TEST_KERNEL_IMG environment variable is not defined.
 func getTestKernelImage() string {
 	envImage := os.Getenv("CTR_TEST_KERNEL_IMG")
 	if envImage != "" {
@@ -203,6 +278,7 @@ type testImageMountOptions struct {
 	Expected  bool
 }
 
+// testImageMounted checks if we can mount an image.
 func testImageMounted(ctx context.Context, imageSvc ports.ImageService, opts testImageMountOptions) {
 	mounted, err := imageSvc.IsMounted(ctx, &ports.ImageMountSpec{
 		ImageName:    opts.ImageName,
