5f8d70c4a5812ab426c68e8e904acb9c379e7a09
Balazs Nadasdi <balazs@weave.works>
2021-12-17 12:12:50 +0100

    Add json tags on cloudinit Metadata (#319)

--- client/cloudinit/metadata.go
+++ client/cloudinit/metadata.go
@@ -1,8 +1,8 @@
 package cloudinit
 
 type Metadata struct {
-	InstanceID    string `yaml:"instance_id"`
-	LocalHostname string `yaml:"local_hostname"`
-	Platform      string `yaml:"platform"`
-	ClusterName   string `yaml:"cluster_name"`
+	InstanceID    string `yaml:"instance_id",json:"instance_id"`
+	LocalHostname string `yaml:"local_hostname",json:"local_hostname"`
+	Platform      string `yaml:"platform",json:"platform"`
+	ClusterName   string `yaml:"cluster_name",json:"cluster_name"`
 }
