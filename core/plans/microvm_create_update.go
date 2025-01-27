60f4888c9ef06bf2c176f659718a261b756586e2
Steve Fraser <steve@cast.ai>
2025-01-27 18:58:39 -0500

    fix imports lint

--- core/plans/microvm_create_update.go
+++ core/plans/microvm_create_update.go
@@ -153,7 +153,6 @@ func (p *microvmCreateOrUpdatePlan) addImageSteps(ctx context.Context,
 	vm *models.MicroVM,
 	imageSvc ports.ImageService,
 ) error {
-	// TODO: Refactory for VirtioFS RootVol Support
 	rootStatus, ok := vm.Status.Volumes[vm.Spec.RootVolume.ID]
 	if !ok {
 		rootStatus = &models.VolumeStatus{}
